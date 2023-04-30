1eb2839dff7e0a5a66fe97c5c9dfe7693564c577
Tech-Monica <monicapatel309@gmail.com>
2023-04-30 13:22:49 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import  Home  from './component/Home';
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+
+import Sidenav from './Sidenav'
+import BookList from './component/BookList';
+import AddBook from './component/AddBook';
+import EditBook from './component/EditBook';
+
+function App() {
+  return (
+    <div className="App">
+      <Sidenav />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        {/* <Route path='/about' element={<About />} /> */}
+        <Route path='/books' element={<BookList />} />
+        <Route path='/addbook' element={<AddBook />} />
+        <Route path='/books/edit/:id' element={<EditBook />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+
+function Home() {
+  return (
+    <div>
+    <Box height={100} />
+    <div>
+    <img className='libImage'
+    src="https://img.freepik.com/free-vector/library-with-bookcases-ladder-chair-lamp_107791-3023.jpg?size=626&ext=jpg&ga=GA1.2.273596903.1677777173&semt=robertav1_2_sidr" alt='Library' />
+    </div>
+    </div>
+  )
+}
+
+export default Home
